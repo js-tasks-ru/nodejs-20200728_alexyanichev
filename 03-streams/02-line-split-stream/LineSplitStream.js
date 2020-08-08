6c82b5df90e10279e122318c2a17c151460c74dc
Alex Yanichev <yanichev85@gmail.com>
2020-08-08 15:03:02 +0300

    add solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,20 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.data = "";
   }
 
   _transform(chunk, encoding, callback) {
+    this.data += chunk;
+    callback()
   }
 
   _flush(callback) {
+    const data_array = this.data.split(os.EOL);
+    for (const item of data_array) {
+      this.push(item)
+    }
+    callback()
   }
 }
 
