20261aeb0fc80d8c3550a27b6f99ec2d9b6b5972
Alex Yanichev <yanichev85@gmail.com>
2020-09-03 17:20:21 -0400

    add solution for 05-http-server-streams 02-file-server-post

---
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -0,0 +1,6 @@
+const Category = require("../models/Category")
+
+module.exports.categoryList = async function categoryList(ctx, next) {
+  const categories = await Category.find({});
+  ctx.body = {categories};
+};
