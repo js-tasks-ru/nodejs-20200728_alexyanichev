553b3a2e1d3cf2907ea608715f62f469c1c2a3ec
Alex Yanichev <yanichev85@gmail.com>
2020-09-04 12:26:44 -0400

    add solution for 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,6 +1,15 @@
 const Category = require("../models/Category")
 
+const formatCategory = category => ({
+  id: category.id,
+  title: category.title,
+  subcategories: category.subcategories.map(subcategory => ({
+    id: subcategory.id,
+    title: subcategory.title,
+  })),
+});
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  const categories = await Category.find({});
-  ctx.body = {categories};
+  const categories = await Category.find();
+  ctx.body = {categories: categories.map(formatCategory)};
 };
--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,45 @@
+const mongoose = require('mongoose');
+const Product = require("../models/Product");
+
+const formatProduct = product => ({
+  id: product.id,
+  title:  product.title,
+  description: product.description,
+  price: product.price,
+  category: product.category,
+  subcategory: product.subcategory,
+  images: product.images,
+});
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategory_id = ctx.request.query.subcategory;
+  if (mongoose.Types.ObjectId.isValid(subcategory_id)) {
+    const products = await Product.find({subcategory: subcategory_id});
+    ctx.body = {products: products.map(formatProduct)};
+  } else {
+    return next()
+  }
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find();
+  ctx.body = {products: products.map(formatProduct)}
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const id = ctx.params.id;
+
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const product = await Product.findById(id);
+    if (product) {
+      ctx.body = {product: formatProduct(product)};
+    } else {
+      ctx.status = 404;
+      ctx.body = {};
+    }
+  } else {
+    ctx.status = 400;
+    ctx.body = {};
+  }
 };
 
